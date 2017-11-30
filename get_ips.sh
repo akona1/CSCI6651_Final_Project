620f766e601748836cf2dc8e0b686d5cb6fd52dc
avinash konatala <34048093+akona1@users.noreply.github.com>
2017-11-29 19:23:40 -0500

    Update get_ips.sh

--- get_ips.sh
+++ get_ips.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
-###############################################################
+###################################################################
 # $Id: get_ips.sh,v 1.2 2017/11/27 02:58:24 root Exp root $
-# Purpose: Fetch denied IP addrs and write to file
-###############################################################
+# Purpose: Parses firewall denials in /var/log/messages and writes
+#          output a text file which is read by update_db.py .
+###################################################################
 # tmp file 
 TMPFL="/tmp/hostile_ips.txt"
 
 # read log file for Firewalld
 LOGFL="/var/log/messages"
 
-# mon day time hostname srcIP dstPort
+# the next one line of shell commands parses all the denials and
+# results in 4 space-delimited fields: Month Day Time sourceIP
+#  1. grep FINAL_REJECT from /var/log/messages
+#  2. capture the 1st, 2nd, 3rd, and 10th fields
+#  3. get rid of "SRC=" in the 10th field
+#  4. sort the output by IP (4th field)
+#  5. eliminate duplcate IP's 
 grep "FINAL_REJECT" $LOGFL | awk '{ print $1 " " $2 " " $3 " " $10 }' | sed 's/SRC=//' | sort -k 4 | uniq --skip-fields=3  > $TMPFL
 
 # exit gracefully with a return code of 0
